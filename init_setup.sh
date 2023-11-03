337655e1e04342803d1bb210da5ac8b58db070ae
SUSHANTH <sushanth172308@gmail.com>
2023-11-03 15:43:23 +0100

    initial setup

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "START"
 echo [$(date)]: "Creating conda env with python 3.9" # change py version as per your need
-conda create --prefix ./env python=3.9 -y
+conda create --prefix ./langchain_env python=3.9 -y
 echo [$(date)]: "activate env"
-source activate ./env
+source activate ./langchain_env
 echo [$(date)]: "installing the requirements" 
 pip install -r requirements.txt
 echo [$(date)]: "END" 
